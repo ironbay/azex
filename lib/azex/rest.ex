d419eb34c1b8b03ca41cb3de1c9e78af9c3daf7d
Dax Ravi <d@ironbay.digital>
2017-05-08 07:52:11 -0400

    Reload config

--- lib/azex/rest.ex
+++ lib/azex/rest.ex
@@ -1,18 +1,17 @@
 defmodule Azex.Rest do
 	alias HTTPipe.Conn
 
-	@account Application.get_env(:azex, :account)
-	@key Application.get_env(:azex, :key) |> Base.decode64!
 
 	def exec(resource, method \\ "GET", body \\ nil, headers \\ []) do
+		{account, key} = config()
 		resource = URI.encode(resource, &URI.char_unreserved?/1)
 		date = :httpd_util.rfc1123_date()
-		signature = "#{date}\n/#{@account}/#{resource}" |> IO.inspect
-		signature = :crypto.hmac(:sha256, @key, signature) |> Base.encode64
-		url = "http://#{@account}.table.core.windows.net/#{resource}"
+		signature = "#{date}\n/#{account}/#{resource}" |> IO.inspect
+		signature = :crypto.hmac(:sha256, key, signature) |> Base.encode64
+		url = "http://#{account}.table.core.windows.net/#{resource}"
 		conn =
 			Conn.new
-			|> Conn.put_req_header("Authorization", "SharedKeyLite #{@account}:#{signature}")
+			|> Conn.put_req_header("Authorization", "SharedKeyLite #{account}:#{signature}")
 			|> Conn.put_req_header("x-ms-date", date)
 			|> Conn.put_req_header("x-ms-version", "2016-05-31")
 			|> Conn.put_req_header("Content-Type", "application/json")
@@ -32,4 +31,11 @@ defmodule Azex.Rest do
 		|> Map.get(:body)
 		|> Poison.decode!
 	end
+
+	defp config do
+		account = Application.get_env(:azex, :account)
+		key = Application.get_env(:azex, :key) |> Base.decode64!
+		{account, key}
+	end
+
 end
